39eb16ce203e0ebd32e5bf30eec7bd28951f4d65
jabarjung <jabarjung@yahoo.ca>
2019-03-07 17:42:48 -0800

    Minor change in the first test.

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
 var rectangle = require('../app/app.js');
 describe('testingRectangleClass', function () {
-  it('should assert the area of the rectangle', function(done) {
+  it('should assert if the area of the rectangle is greater than 100', function(done) {
     // Assertion goes here
-    var square = new rectangle(10, 10);
-    assert.ok(square.area === 100);
+    var square = new rectangle(10, 20);
+    assert.ok(square.area > 100);
     done();
   });
   it('should check if it is a square', function(done) {
